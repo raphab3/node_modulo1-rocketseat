db010cb154ba2032be5079fce03e630088f15cc1
Rafael Batista Santos <raphab33@gmail.com>
2019-08-21 17:02:42 -0300

    CRUD com array

--- index.js
+++ index.js
@@ -2,9 +2,41 @@ const express = require("express");
 
 const server = express();
 
-server.get("/teste/:id", (req, res) => {
-  const { id } = req.params;
-  return res.json({ mensage: `Rapha NODE ${id}` });
+server.use(express.json());
+
+const users = ["Rafael", "Renato", "Rodrigo"];
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+  return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { name } = req.body;
+  const { index } = req.params;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.json(users);
 });
 
 server.listen(3000);
