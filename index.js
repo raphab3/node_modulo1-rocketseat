b3afc5725c148804a4331ffec70914534a2626fb
Rafael Batista Santos <raphab33@gmail.com>
2019-08-21 16:03:25 -0300

    query, route e params

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const server = express();
 
-server.get("/teste", (req, res) => {
-  return res.json({ mensage: "Rapha NODE" });
+server.get("/teste/:id", (req, res) => {
+  const { id } = req.params;
+  return res.json({ mensage: `Rapha NODE ${id}` });
 });
 
 server.listen(3000);
